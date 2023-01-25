b2e2950110ee635e9341c6e035ec53b2f1c6edf1
Md Ariful Islam <gmarifulislamarif@gmail.com>
2023-01-25 19:21:21 +0600

    result data reading updated

--- data_map.rb
+++ data_map.rb
@@ -1,7 +1,6 @@
 require 'pg'
 require 'csv'
 require "fileutils"
-require 'json'
 
 class DataMap
   def self.call(limit, offset)
@@ -19,40 +18,35 @@ class DataMap
 
   files = Dir["#{@file_path}/*"]
 
-  files.each do |file|
-    CSV.foreach(file, :headers => true) do |row|
-      data = row.to_h
-      next if data['CUSTOMER_NAME'].nil?
+    files.each do |file|
+      CSV.foreach(file, :headers => true) do |row|
+        data = row.to_h
+        next if data['CUSTOMER_NAME'].nil?
 
-      splited_words = data['CUSTOMER_NAME'].strip.split(' ')
-      name_array = splited_words.reject(&:empty?)
-      is_missing = false
-      result_array = name_array.map do |name_part|
-        result = connection.exec("SELECT dictionaries.id AS id, dictionaries.english_word AS english_word, dictionaries.burmese_word AS burmese_word
+        splited_words = data['CUSTOMER_NAME'].strip.split(' ')
+        name_array = splited_words.reject(&:empty?)
+        is_missing = false
+        result_array = name_array.map do |name_part|
+          result = connection.exec("SELECT dictionaries.id AS id, dictionaries.english_word AS english_word, dictionaries.burmese_word AS burmese_word
                                   FROM dictionaries
                                   WHERE dictionaries.english_word = '#{name_part}' AND word_type = 3
-                                  limit #{1} offset #{offset};")
-
-        if result.to_h.nil? || result.to_h.count.zero?
-          is_missing = true
-          connection.exec("INSERT INTO missing_words (english_word)
+                                  limit #{limit};")
+          if result.count == 0
+            is_missing = true
+            connection.exec("INSERT INTO missing_words (english_word)
           VALUES ('#{name_part}');")
-        elsif
-          result.to_h['burmese_word']
-        else
-          name_part
+            ''
+          else
+            result[0]['burmese_word']
+          end
         end
+        connection.exec("INSERT INTO customers (customer_name, translated_name, is_missing)
+              VALUES ('#{data['CUSTOMER_NAME']}', '#{result_array.join(' ')}', #{is_missing});")
       end
-
-      rest_result = result_array.length.zero? ? ' ' : result_array.map{|r| r.to_h['burmese_word'].nil? ? ' ': r.to_h['burmese_word']}
-      connection.exec("INSERT INTO customers (customer_name, translated_name, is_missing)
-              VALUES ('#{data['CUSTOMER_NAME']}', '#{rest_result.join(' ')}', #{is_missing});")
     end
-  end
-  connection.close if connection
-  puts "Closing database connection for the database : #{@source_db_name}"
-
+    connection.close if connection
+    puts "Closing database connection for the database : #{@source_db_name}"
   end
 end
 
-DataMap.call(5, 1)
+DataMap.call(1, 1)
